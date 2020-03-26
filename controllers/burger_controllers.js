0de7c5f7e7ec80d3c959400fff94197fb9319214
jimmy nagles <jimmynagles@gmail.com>
2020-03-25 18:37:24 -0700

    burger controllers

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -6,9 +6,9 @@ var burger = require("../models/burger");
 var router = express.Router();
 
 
-// Create all our routes and set up logic within those routes where required.
+// Create selectAll our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       cats: data
     };
@@ -17,26 +17,24 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  burger.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devored"],
+   [req.body.burger_name, req.body.devored],
+    function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+   devored: req.body.devored
   }, condition, function(result) {
-    if (result.changedRows == 0) {
+    if (result.changedRows === 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
@@ -45,17 +43,17 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+    if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
 });
 
 // Export routes for server.js to use.
